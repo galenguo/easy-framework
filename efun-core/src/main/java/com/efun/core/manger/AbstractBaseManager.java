e7e927be760c9bc22798d9e4b12c6cf838baa4aa
Galen <0113101191galen>
2017-05-15 17:42:23 +0800

    增加manager基类； 修改日志配置；

---
+++ efun-core/src/main/java/com/efun/core/manger/AbstractBaseManager.java
@@ -0,0 +1,14 @@
+package com.efun.core.manger;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * AbstractBaseManager
+ *
+ * @author Galen
+ * @since 2017/5/15
+ */
+public abstract class AbstractBaseManager {
+    protected final Logger logger = LogManager.getLogger(this.getClass());
+}
